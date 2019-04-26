6563e2e343a63e6147b7e951742f9c3f3ddf30d1
sarabolindberg <49994750+sarabolindberg@users.noreply.github.com>
2019-04-26 19:00:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ else if (element.src.match(/*3*/"https://raw.githubusercontent.com/sarabolindber
   element.src=/*4*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede4.jpg";
          
   }
-  else if (element.src.match(/*2*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede2.jpg")){
+  else if (element.src.match(/*2*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/Image2.jpg?raw=true")){
     element.src=/*3*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede3.jpg"
   }
   
   else {
-    element.src=/*2*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede2.jpg";
+    element.src=/*2*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/Image2.jpg?raw=true";
   }
 }

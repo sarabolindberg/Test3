c7078d500f6e66ff9f184b0b463e1a26df272880
sarabolindberg <49994750+sarabolindberg@users.noreply.github.com>
2019-04-26 15:53:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 element=document.getElementById('example')
 if (element.src.match(/*4*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58570860_109261623613744_4961473957514444800_n.jpg?_nc_cat=103&_nc_ht=scontent.fhfa2-2.fna&oh=17b3158bb2046059de88afa65f89364f&oe=5D72E404"))
   {
-  element.src=/*1*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/image_1.jpg?raw=true";
+  element.src=/*1*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/Unavngivet%201.jpg?raw=true";
     ;
   }
 else if (element.src.match(/*3*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58113474_109261613613745_8054468769960951808_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=726350ee18eb760c0d06f8ecfc58c584&oe=5D336605"))

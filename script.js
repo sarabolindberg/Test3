067ea6e62c9768af3245ce45ca4bab77f91dbf26
sarabolindberg <49994750+sarabolindberg@users.noreply.github.com>
2019-04-26 15:27:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 element=document.getElementById('example')
 if (element.src.match("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58570860_109261623613744_4961473957514444800_n.jpg?_nc_cat=103&_nc_ht=scontent.fhfa2-2.fna&oh=17b3158bb2046059de88afa65f89364f&oe=5D72E404"))
   {
-  element.src="https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58374508_109275966945643_7631695777303101440_n.jpg?_nc_cat=110&_nc_ht=scontent.fhfa2-2.fna&oh=5994484464dea8892880953044577909&oe=5D2E8DE7";
+  element.src="https://imgur.com/EnhJddO";
     ;
   }
 else if (element.src.match("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58113474_109261613613745_8054468769960951808_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=726350ee18eb760c0d06f8ecfc58c584&oe=5D336605"))

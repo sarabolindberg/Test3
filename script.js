78a55523cb1b7247c3080290582a6710d0ab67ac
sarabolindberg <49994750+sarabolindberg@users.noreply.github.com>
2019-04-26 19:07:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
   function changeImage()
 {
 element=document.getElementById('example')
-if (element.src.match(/*4*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede4.jpg"))
+if (element.src.match(/*4*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/image4.jpg"))
   {
   element.src=/*1*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/test1.jpg?raw=true";
     ;
   }
-else if (element.src.match(/*3*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede3.jpg"))
+else if (element.src.match(/*3*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/image3.jpg?raw=true"))
   {
          
-  element.src=/*4*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede4.jpg";
+  element.src=/*4*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/image4.jpg";
          
   }
-  else if (element.src.match(/*2*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/Image2.jpg?raw=true")){
-    element.src=/*3*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede3.jpg"
+  else if (element.src.match(/*2*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/image2.jpg")){
+    element.src=/*3*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/image3.jpg?raw=true"
   }
   
   else {
-    element.src=/*2*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/Image2.jpg?raw=true";
+    element.src=/*2*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/image2.jpg";
   }
 }

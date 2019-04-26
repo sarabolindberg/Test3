81f51fdf826df6fc17715d9b737f06e8b160682a
sarabolindberg <49994750+sarabolindberg@users.noreply.github.com>
2019-04-26 16:01:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
   function changeImage()
 {
 element=document.getElementById('example')
-if (element.src.match(/*4*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58570860_109261623613744_4961473957514444800_n.jpg?_nc_cat=103&_nc_ht=scontent.fhfa2-2.fna&oh=17b3158bb2046059de88afa65f89364f&oe=5D72E404"))
+if (element.src.match(/*4*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede4.jpg"))
   {
   element.src=/*1*/"https://github.com/sarabolindberg/Til-Joanna/blob/master/Unavngivet%201.jpg?raw=true";
     ;
   }
-else if (element.src.match(/*3*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58113474_109261613613745_8054468769960951808_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=726350ee18eb760c0d06f8ecfc58c584&oe=5D336605"))
+else if (element.src.match(/*3*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede3.jpg"))
   {
          
-  element.src=/*4*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58570860_109261623613744_4961473957514444800_n.jpg?_nc_cat=103&_nc_ht=scontent.fhfa2-2.fna&oh=17b3158bb2046059de88afa65f89364f&oe=5D72E404";
+  element.src=/*4*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede4.jpg";
          
   }
-  else if (element.src.match(/*2*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58727341_109261616947078_6033638484839759872_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=19806ccec723af20559b0665ec8a7ae0&oe=5D75A592")){
-    element.src=/*3*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58113474_109261613613745_8054468769960951808_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=726350ee18eb760c0d06f8ecfc58c584&oe=5D336605"
+  else if (element.src.match(/*2*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede2.jpg")){
+    element.src=/*3*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede3.jpg"
   }
   
   else {
-    element.src=/*2*/"https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58727341_109261616947078_6033638484839759872_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=19806ccec723af20559b0665ec8a7ae0&oe=5D75A592";
+    element.src=/*2*/"https://raw.githubusercontent.com/sarabolindberg/Til-Joanna/master/billede2.jpg";
   }
 }

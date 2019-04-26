574d3ae865dab00a53beab153d98806720e6572b
sarabolindberg <49994750+sarabolindberg@users.noreply.github.com>
2019-04-26 15:16:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
   function changeImage()
 {
 element=document.getElementById('example')
-if (element.src.match("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/1393736_10152001804709359_1707936580_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=0038bb7a8fe0895f214011337651a9a6&oe=5D6D9AFB"))
+if (element.src.match("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58570860_109261623613744_4961473957514444800_n.jpg?_nc_cat=103&_nc_ht=scontent.fhfa2-2.fna&oh=17b3158bb2046059de88afa65f89364f&oe=5D72E404"))
   {
   element.src="https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58374508_109275966945643_7631695777303101440_n.jpg?_nc_cat=110&_nc_ht=scontent.fhfa2-2.fna&oh=5994484464dea8892880953044577909&oe=5D2E8DE7";
     ;
   }
-else if (element.src.match("http://www.digitalmusicnews.com/wp-content/uploads/2017/05/psy_BTS.jpg"))
+else if (element.src.match("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58113474_109261613613745_8054468769960951808_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=726350ee18eb760c0d06f8ecfc58c584&oe=5D336605"))
   {
          
-  element.src="https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/1393736_10152001804709359_1707936580_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=0038bb7a8fe0895f214011337651a9a6&oe=5D6D9AFB";
+  element.src="https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58570860_109261623613744_4961473957514444800_n.jpg?_nc_cat=103&_nc_ht=scontent.fhfa2-2.fna&oh=17b3158bb2046059de88afa65f89364f&oe=5D72E404";
          
   }
-  else if (element.src.match("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kpop-1550876387.png")){
-    element.src="http://www.digitalmusicnews.com/wp-content/uploads/2017/05/psy_BTS.jpg"
+  else if (element.src.match("https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58727341_109261616947078_6033638484839759872_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=19806ccec723af20559b0665ec8a7ae0&oe=5D75A592")){
+    element.src="https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58113474_109261613613745_8054468769960951808_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=726350ee18eb760c0d06f8ecfc58c584&oe=5D336605"
   }
   
   else {
-    element.src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/kpop-1550876387.png";
+    element.src="https://scontent.fhfa2-2.fna.fbcdn.net/v/t1.0-9/58727341_109261616947078_6033638484839759872_n.jpg?_nc_cat=108&_nc_ht=scontent.fhfa2-2.fna&oh=19806ccec723af20559b0665ec8a7ae0&oe=5D75A592";
   }
 }
 
